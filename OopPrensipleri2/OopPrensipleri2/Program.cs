90545a8d4fe6f8294780fc7b493e02d8e56e22b9
Vuslat <vuslatnur1234@gmail.com>
2026-04-07 15:47:12 +0300

    06-04-2026

---
+++ OopPrensipleri2/OopPrensipleri2/Program.cs
@@ -0,0 +1,35 @@
+﻿OduncIslemi islem = new OduncIslemi();
+
+islem.KitapAd = "Fahrenheit";
+islem.UyeAd = "Ali Şahin";
+
+islem.EkranaYazdir();
+
+abstract class Kitap // Soyutlama (Abstraction)
+{
+    private string kitapAd; // Kapsülleme (Encapsulation)
+
+    public string KitapAd
+    {
+        get { return kitapAd; }
+        set { kitapAd = value; }
+    }
+
+    public abstract void EkranaYazdir(); // Soyut metod
+}
+
+class OduncIslemi : Kitap // Kalıtım (Inheritance)
+{
+    private string uyeAd; // Kapsülleme
+
+    public string UyeAd
+    {
+        get { return uyeAd; }
+        set { uyeAd = value; }
+    }
+
+    public override void EkranaYazdir() // Çok biçimlilik (Polymorphism) - Override
+    {
+        Console.WriteLine(UyeAd + " adlı üye " + KitapAd + " kitabını ödünç aldı.");
+    }
+}
